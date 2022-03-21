d7da3d98852b22c4b47414202e589925c18cf5ad
mrcxmrj <marcin.zurawel@gmail.com>
2022-03-21 18:10:07 +0100

    added pokemon fetching

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+
+const getCachedPokemons = () => JSON.parse(localStorage.getItem("pokemonList"));
 
 function App() {
-    return (
+    const [loading, setLoading] = useState(false);
+    const [pokemonList, setPokemonList] = useState(getCachedPokemons());
+
+    useEffect(() => {
+        const cachedPokemons = getCachedPokemons();
+
+        if (!cachedPokemons) {
+            setLoading(true);
+            fetch("https://pokeapi.co/api/v2/pokemon")
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw response;
+                    }
+                })
+                .then((data) => {
+                    console.log(data);
+                    localStorage.setItem("pokemonList", JSON.stringify(data));
+                    setPokemonList(data.results);
+                    setLoading(false);
+                })
+                .catch((error) => console.error(error));
+        }
+    }, [setLoading, setPokemonList]);
+
+    return loading ? (
+        "loading..."
+    ) : (
         <div className="container">
             <h1>Pokedex</h1>
             <div className="list">
@@ -18,6 +48,8 @@ function App() {
                         <Pokemon />
                     </li>
                 </ul>
+                {JSON.stringify(pokemonList)}
+                <button>load more</button>
             </div>
         </div>
     );
@@ -28,7 +60,7 @@ const Pokemon = (props) => {
 };
 
 const Searchbox = (props) => {
-    return <>search, sort by, filter</>;
+    return <>search, sort by</>;
 };
 
 export default App;
